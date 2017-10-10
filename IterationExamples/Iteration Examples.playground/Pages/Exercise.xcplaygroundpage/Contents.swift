72c5f9a2c53216859bda5e21b670396523fbbbdc
Gao Kevin <kgao@lcs-mba3111.lcs.on.ca>
2017-10-10 09:47:09 -0400

    Done the exercise.

--- IterationExamples/Iteration Examples.playground/Pages/Exercise.xcplaygroundpage/Contents.swift
+++ IterationExamples/Iteration Examples.playground/Pages/Exercise.xcplaygroundpage/Contents.swift
@@ -26,12 +26,16 @@ import PlaygroundSupport
 //: ## Add your code below
 
 // Create canvas
-let canvas = Canvas(width: 300, height: 300)
+let c = Canvas(width: 300, height: 300)
 
 // Below this line, try combining a loop and a single statement to generate the goal
 
+//loop
+for y in stride(from: 0, to: 300, by: 20){
+    c.drawLine(fromX: 0+y, fromY: 0, toX: 300, toY: y)
+}
 /*:
  ## Template code
  The code below is necessary to see results in the Assistant Editor at right. Please do not remove.
  */
-PlaygroundPage.current.liveView = canvas.imageView
+PlaygroundPage.current.liveView = c.imageView
