59dda7a1c70f868c263226a9676245fb99eaf643
Gao Kevin <kgao@lcs-mba3111.lcs.on.ca>
2017-10-12 13:26:28 -0400

    Draw dots

--- IterationExamples/Iteration Examples.playground/Pages/Challenge.xcplaygroundpage/Contents.swift
+++ IterationExamples/Iteration Examples.playground/Pages/Challenge.xcplaygroundpage/Contents.swift
@@ -17,15 +17,15 @@ let c = Canvas(width: 300, height: 300)
 // Below this line, try combining a loop and four statements that draw lines to generate the goal
 
 //loop
-for i in stride(from: 0, to: 300, by: 20){
-
-//draw lines
-    c.drawLine(fromX: i, fromY: 0, toX: 300, toY: i)
-    c.drawLine(fromX: 0, fromY: 0+i, toX: i, toY: 300)
-    c.drawLine(fromX: 300, fromY: i, toX: 300-i, toY:300)
-    c.drawLine(fromX: 300-i, fromY: 0, toX: 0, toY: i)
-   
+for x in stride(from: 25, to: 276, by: 50){
+    for y in stride(from: 275, to: 24, by: -50){
+    
+//draw dots
+    c.drawEllipse(centreX: x, centreY: y, width: 3, height: 3, borderWidth: 0)
+    }
 }
+
+
 /*:
  ## Template code
  The code below is necessary to see results in the Assistant Editor at right. Please do not remove.
